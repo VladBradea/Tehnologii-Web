caf27c28c8e0c978558ab6c6ebde35de88a197d2
VladBradea <vlad.bradea02@e-uvt.ro>
2023-11-03 18:07:56 +0200

    backend generated

---
+++ Backend/quizGrade/src/main/java/com/quizGrade/quizGrade/QuizGradeApplication.java
@@ -0,0 +1,13 @@
+package com.quizGrade.quizGrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGradeApplication.class, args);
+	}
+
+}
---
+++ Backend/quizGrade/src/test/java/com/quizGrade/quizGrade/QuizGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizGrade.quizGrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
