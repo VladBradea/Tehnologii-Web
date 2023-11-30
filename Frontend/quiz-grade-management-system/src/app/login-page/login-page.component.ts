cbcefad7a73708617dbf0f2e7d7895fbcd88ebcd
Ilies Octavian <120681033+OctavianIlies02@users.noreply.github.com>
2023-11-30 17:32:01 +0200

    made login work

--- Frontend/quiz-grade-management-system/src/app/login-page/login-page.component.ts
+++ Frontend/quiz-grade-management-system/src/app/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TeacherService } from '../Services/teacher.service';
+import { StudentService } from '../Services/student.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,4 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  username: string = '';
+  password: string = '';
+  accountNotFound: boolean = false;
+
+  constructor(private teacherService: TeacherService, private studentService: StudentService, private router: Router) {}
+
+  onSubmit(): void {
+    // Try teacher login
+    this.teacherService.getTeachers().subscribe(
+      teachers => {
+        const matchedTeacher = teachers.find(teacher => teacher.email === this.username && teacher.password === this.password);
+        if (matchedTeacher) {
+          console.log('Teacher logged in:', matchedTeacher);
+          this.router.navigate(['/teacher-page']);
+        } else {
+          // If teacher login fails, try student login
+          this.studentService.getStudents().subscribe(
+            students => {
+              const matchedStudent = students.find(student => student.email === this.username && student.password === this.password);
+              if (matchedStudent) {
+                console.log('Student logged in:', matchedStudent);
+                this.router.navigate(['/teacher-page']);
+              } else {
+                console.error('Invalid username or password');
+                this.accountNotFound = true;
+              }
+            },
+            error => {
+              console.error('Error fetching students:', error);
+            }
+          );
+        }
+      },
+      error => {
+        console.error('Error fetching teachers:', error);
+      }
+    );
+  }
 }
