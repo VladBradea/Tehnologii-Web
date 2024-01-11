ea52d2be15d26a14e87a573d1d98479d1978816b
Ilies Octavian <120681033+OctavianIlies02@users.noreply.github.com>
2024-01-11 20:23:56 +0200

    statistics-page

--- Frontend/quiz-grade-management-system/src/app/statistics-page/statistics-page.component.ts
+++ Frontend/quiz-grade-management-system/src/app/statistics-page/statistics-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Exam } from '../Classes/Exam';
+import { ExamService } from '../Services/exam.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-statistics-page',
   templateUrl: './statistics-page.component.html',
   styleUrls: ['./statistics-page.component.css']
 })
-export class StatisticsPageComponent {
+export class StatisticsPageComponent implements OnInit{
+
+  exams: Exam[] = [];
+
+  constructor(private examService: ExamService){}
+
+  ngOnInit(){
+    this.getExams();
+  }
+
+  public getExams(): void{
+    this.examService.getExams().subscribe(
+      (response: Exam[])=>{
+        this.exams = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 
 }
