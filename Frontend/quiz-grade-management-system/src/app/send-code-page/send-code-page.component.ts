6e57b65f033e801072e03ecbda1c2330c46ac32d
VladBradea <vlad.bradea02@e-uvt.ro>
2024-01-11 21:02:47 +0200

    Send code page implemented
    -without css styling

--- Frontend/quiz-grade-management-system/src/app/send-code-page/send-code-page.component.ts
+++ Frontend/quiz-grade-management-system/src/app/send-code-page/send-code-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-send-code-page',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./send-code-page.component.css']
 })
 export class SendCodePageComponent {
+  examId!: string;  // Keep as string to capture form input
 
+  constructor(private router: Router) { }
+
+  onSubmit() {
+    const examIdNumber = Number(this.examId);  // Convert string to number
+    if (!isNaN(examIdNumber)) {
+      this.router.navigate(['/take-exam', { id: examIdNumber }]);
+    } else {
+      // Handle the error if the input is not a valid number
+      alert('Please enter a valid exam ID number');
+    }
+  }
 }
+
