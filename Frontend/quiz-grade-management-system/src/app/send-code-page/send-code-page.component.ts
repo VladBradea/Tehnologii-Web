3889cd070c4d3d3334bec7e06f5e5fa9556255e3
Ilies Octavian <120681033+OctavianIlies02@users.noreply.github.com>
2024-01-12 23:52:51 +0200

    small changes

--- Frontend/quiz-grade-management-system/src/app/send-code-page/send-code-page.component.ts
+++ Frontend/quiz-grade-management-system/src/app/send-code-page/send-code-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ExamService } from '../Services/exam.service';
+import { Exam } from '../Classes/Exam';
 
 @Component({
   selector: 'app-send-code-page',
@@ -7,6 +9,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./send-code-page.component.css']
 })
 export class SendCodePageComponent {
+  enteredExamId: string = '';
+  exam: Exam | null = null;
+
+  constructor(private examService: ExamService, private router: Router) { }
+
+  checkAndOpenExam() {
+    const enteredIdAsNumber = Number(this.enteredExamId);
+    if (!isNaN(enteredIdAsNumber)) {
+      this.examService.getExamById(enteredIdAsNumber).subscribe(
+        exam => {
+          if (exam) {
+            this.exam = exam;
+          } else {
+            console.log('Exam not found');
+          }
+        },
+        error => {
+          if (error.status === 302 && error.error) {
+            this.exam = error.error;
+          }
   
+        }
+      );
+    } else {
+      console.log('Invalid exam ID format');
+    }
+  }
 }
 
