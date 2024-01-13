61655ee035ecd89f3e5eb83a13de24d6da0b93e3
Ilies Octavian <120681033+OctavianIlies02@users.noreply.github.com>
2024-01-13 21:52:11 +0200

    really small changes

--- Frontend/quiz-grade-management-system/src/app/view-grade-dialog/view-grade-dialog.component.ts
+++ Frontend/quiz-grade-management-system/src/app/view-grade-dialog/view-grade-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Grade } from '../Classes/Grade';
 import { Exercise } from '../Classes/Exercise';
 import { Router } from '@angular/router';
@@ -11,13 +11,20 @@ import { UserDataService } from '../Services/user-data.service';
   styleUrls: ['./view-grade-dialog.component.css']
 })
 export class ViewGradeDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { grade: Grade },  private router: Router, private userDataService: UserDataService) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { grade: Grade },  private router: Router, private userDataService: UserDataService, private dialogRef: MatDialogRef<ViewGradeDialogComponent>) {}
 
-  public logout(){
+  public logout() {
     this.userDataService.logout();
+    this.router.navigate(['/quizGrade']);
+    this.closeDialog();
   }
 
   backToMenu() {
     this.router.navigate(['/student-main-page']);
+    this.closeDialog();
+  }
+
+  private closeDialog() {
+    this.dialogRef.close();
   }
 }
